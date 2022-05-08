21892e12be7b208c7dee2b134522b4c1d5bbefba
SeoSeunghun <proto_seo@naver.com>
2022-05-08 16:33:06 +0900

    [feat][#1] Member domain 추가
    - Member domain 추가

---
+++ src/main/java/com/footprint/member/domain/Member.java
@@ -0,0 +1,42 @@
+package com.footprint.member.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private Long id;
+
+	@Column(name = "username", nullable = false, unique = true)
+	private String username;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Column(name = "nickname", nullable = false)
+	private String nickname;
+
+	@Builder
+	public Member(String username, String password, String nickname) {
+		this.username = username;
+		this.password = password;
+		this.nickname = nickname;
+	}
+
+
+}
