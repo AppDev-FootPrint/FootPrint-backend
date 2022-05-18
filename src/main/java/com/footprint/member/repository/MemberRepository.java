3f788f75410afca42462694f93c568d548e4c30b
ShinDongHun1 <huipulci1@naver.com>
2022-05-18 20:32:30 +0900

    [feat] MemberRepository 추가
    - JpaRepository 상속
    - Username을 통해 Member를 조회하는 메서드 추가

--- src/main/java/com/footprint/member/repository/MemberRepository.java
+++ src/main/java/com/footprint/member/repository/MemberRepository.java
@@ -1,4 +1,15 @@
-package com.footprint.member.repository;/**
+package com.footprint.member.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.footprint.member.domain.Member;
+
+/**
  * Created by ShinD on 2022/05/14.
- */public class MemberRepository {
+ */
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByUsername(String username);
 }
