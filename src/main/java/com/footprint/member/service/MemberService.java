1f493b7100f9f00f6859e530822e145740fcba95
ShinDongHun1 <huipulci1@naver.com>
2022-05-18 20:47:36 +0900

    [feat] MemberService 인터페이스와 관련 DTO 추가
    - UserDetailsService 상속받아 해당 인터페이스 구현체가 loadUserByUsername 을 통해 인증을 진행하도록 설정.
    
    - create 메서드 추가
      회원가입 기능 수행

--- src/main/java/com/footprint/member/service/MemberService.java
+++ src/main/java/com/footprint/member/service/MemberService.java
@@ -1,4 +1,14 @@
-package com.footprint.member.service;/**
+package com.footprint.member.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.footprint.member.service.dto.CreateMemberDto;
+
+/**
  * Created by ShinD on 2022/05/14.
- */public interface MemberService {
+ */
+public interface MemberService extends UserDetailsService {
+
+	void create(CreateMemberDto createMemberDto);
+
 }
--- src/main/java/com/footprint/member/service/dto/CreateMemberDto.java
+++ src/main/java/com/footprint/member/service/dto/CreateMemberDto.java
@@ -1,4 +1,34 @@
-package com.footprint.member.service.dto;/**
+package com.footprint.member.service.dto;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.footprint.member.domain.Member;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
  * Created by ShinD on 2022/05/14.
- */public class CreateMemberDto {
+ */
+@Getter
+public class CreateMemberDto {
+
+	private String username;
+	private String password;
+	private String nickName;
+
+	@Builder
+	public CreateMemberDto(String username, String password, String nickName) {
+		this.username = username;
+		this.password = password;
+		this.nickName = nickName;
+	}
+
+	public void encodePassword(PasswordEncoder passwordEncoder) {
+		this.password = passwordEncoder.encode(password);
+	}
+
+	public Member toEntity() {
+		return Member.builder().username(username).password(password).nickname(nickName).build();
+	}
 }
