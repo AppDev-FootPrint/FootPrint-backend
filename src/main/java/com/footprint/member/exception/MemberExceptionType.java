18a609f997615f40b7edf33577e68e0feef7756a
ShinDongHun1 <huipulci1@naver.com>
2022-05-18 20:49:53 +0900

    [feat] MemberServiceImpl, MemberException 추가
    - MemberService를 구현한 MemberServiceImpl 추가
    - Member 관련 예외를 처리할 MemberException 추가

--- src/main/java/com/footprint/member/exception/MemberExceptionType.java
+++ src/main/java/com/footprint/member/exception/MemberExceptionType.java
@@ -1,4 +1,41 @@
-package com.footprint.member.exception;/**
+package com.footprint.member.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.footprint.common.exception.BaseExceptionType;
+
+/**
  * Created by ShinD on 2022/05/14.
- */public class MemberExceptionType {
+ */
+public enum MemberExceptionType implements BaseExceptionType {
+
+	NOT_FOUND(100, HttpStatus.NOT_FOUND, "회원이 존재하지 않습니다."),
+	DUPLICATE_USERNAME(101, HttpStatus.CONFLICT,"이미 존재하는 아이디입니다.");
+
+
+
+	private final int errorCode;
+	private final HttpStatus httpStatus;
+	private final String errorMessage;
+
+	MemberExceptionType(int errorCode, HttpStatus httpStatus, String errorMessage) {
+		this.errorCode = errorCode;
+		this.httpStatus = httpStatus;
+		this.errorMessage = errorMessage;
+	}
+
+	@Override
+	public int getErrorCode() {
+		return this.errorCode;
+	}
+
+	@Override
+	public HttpStatus getHttpStatus() {
+		return this.httpStatus;
+	}
+
+	@Override
+	public String getErrorMessage() {
+		return this.errorMessage;
+	}
 }
--- src/main/java/com/footprint/member/service/MemberServiceImpl.java
+++ src/main/java/com/footprint/member/service/MemberServiceImpl.java
@@ -1,4 +1,74 @@
-package com.footprint.member.service;/**
+package com.footprint.member.service;
+
+import static com.footprint.member.exception.MemberExceptionType.*;
+
+import java.util.ArrayList;
+
+import javax.transaction.Transactional;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.footprint.member.domain.Member;
+import com.footprint.member.exception.MemberException;
+import com.footprint.member.repository.MemberRepository;
+import com.footprint.member.service.dto.CreateMemberDto;
+
+import lombok.RequiredArgsConstructor;
+
+/**
  * Created by ShinD on 2022/05/14.
- */public class MemberServiceImpl {
+ */
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService{
+
+	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	/**
+	 * 회원가입
+	 */
+	@Override
+	public void create(CreateMemberDto createMemberDto) {
+
+		//TODO 비밀번호를 암호화 하는 책임을 Member 에 둘지, encodePassword 에 둘지 고민
+		createMemberDto.encodePassword(passwordEncoder);
+
+		Member member = createMemberDto.toEntity();
+
+		checkDuplicateUsername(member.getUsername());
+
+		memberRepository.save(member);
+	}
+
+
+
+	/**
+	 * 아이디 중복여부 체크
+	 * 중복되었을 경우 MemberException 발생
+	 */
+	private void checkDuplicateUsername(String username) {
+		memberRepository.findByUsername(username).ifPresent((member)-> {
+			throw new MemberException(DUPLICATE_USERNAME);
+		} );
+	}
+
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Member member = memberRepository.findByUsername(username)
+										.orElseThrow(() -> new UsernameNotFoundException("회원을 찾을 수 없습니다."));
+
+		return User.builder()
+			.username(username)
+			.password(member.getPassword())
+			.authorities(new ArrayList<>()) //TODO 어드민 등의 관리자 계정이 따로 필요하다면 수정
+			.build();
+	}
 }
