ca97f10fad7a399e36ce2b35e5b82fc12d610703
ShinDongHun1 <huipulci1@naver.com>
2022-05-19 20:25:42 +0900

    [fix] JwtAuthenticationFilter가 OncePerRequestFilter를 상속받도록 수정
    - 기존 Filter를 구현하였을 때 필터가 두번 작동되는 오류가 있었음
        - 원인은 아직 모르겠음
        - 한번만 작동하도록 OncePerRequestFilter를 상속받아 구현함

--- src/main/java/com/footprint/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/footprint/auth/filter/JwtAuthenticationFilter.java
@@ -2,19 +2,20 @@ package com.footprint.auth.filter;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.footprint.auth.jwt.JwtService;
 import com.footprint.member.domain.Member;
@@ -26,24 +27,26 @@ import lombok.RequiredArgsConstructor;
  * Created by ShinD on 2022/05/18.
  */
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter implements Filter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {//TODO Filter 를 상속받으면 두번 작동함
 
 	private final JwtService jwtService;
 	private final MemberRepository memberRepository;
 
+	private final List<String> NO_CHECK_URL = Arrays.asList("/login", "/signUp");
+
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
 		/*
-		요청 정보로부터 인증을 시도함. 인증 시 SecurityContextHolder 에 인증정보 저장
+			요청 정보로부터 인증을 시도함. 인증 시 SecurityContextHolder 에 인증정보 저장
 		 */
 		executeAuthentication((HttpServletRequest)request);
 
-		chain.doFilter(request,response);//필수!
+		if(SecurityContextHolder.getContext().getAuthentication() == null) {
+			filterChain.doFilter(request, response);
+		}
 	}
-
-
 
 	private void executeAuthentication(HttpServletRequest request) {
 		jwtService.extractToken(request)
@@ -58,7 +61,6 @@ public class JwtAuthenticationFilter implements Filter {
 
 	private void saveSecurityContextHolder(Member member) {
 		SecurityContext context = SecurityContextHolder.createEmptyContext();
-
 		UserDetails userDetails = User.builder()
 			.username(member.getId().toString())//UserDetails 의 username 에 id 값을 넣어줌
 			.password(member.getPassword())//이거 안해주면 오류남!
