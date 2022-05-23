70ccefc5db7d862f436b99c1f89320f17b91c7ef
SeoSeunghun <proto_seo@naver.com>
2022-05-23 19:36:23 +0900

    [test][#4] CreatedAt 관련 수정

--- src/test/java/com/footprint/maintravel/fixture/MainTravelFixture.java
+++ src/test/java/com/footprint/maintravel/fixture/MainTravelFixture.java
@@ -1,6 +1,9 @@
 package com.footprint.maintravel.fixture;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.footprint.maintravel.controller.dto.MainTravelRequest;
 import com.footprint.maintravel.controller.dto.MainTravelResponse;
@@ -17,19 +20,22 @@ public class MainTravelFixture {
 	public static final LocalDate START_DATE = LocalDate.parse(START_DATE_STRING);
 	public static final String END_DATE_STRING = "2022-03-17";
 	public static final LocalDate END_DATE = LocalDate.parse(END_DATE_STRING);
+	public static final LocalDateTime CREATED_AT = LocalDateTime.of(2022, 3, 18, 10, 10);
+	public static final String CREATED_AT_STRING = CREATED_AT.toString();
 	public static final Boolean IS_VISIBLE = true;
 	public static final Boolean IS_COMPLETED = true;
 	public static final int LIKE_NUM = 0;
 
 	public static MainTravel getMainTravel() {
-		return MainTravel.builder()
+		MainTravel mainTravel = MainTravel.builder()
 			.title(TITLE)
 			.startDate(START_DATE)
 			.endDate(END_DATE)
 			.isVisible(IS_VISIBLE)
 			.isCompleted(IS_COMPLETED)
 			.build();
-
+		ReflectionTestUtils.setField(mainTravel, "createdAt", CREATED_AT);
+		return mainTravel;
 	}
 
 	public static MainTravelRequest getMainTravelRequest() {
@@ -37,13 +43,13 @@ public class MainTravelFixture {
 	}
 
 	public static MainTravelResponse getMainTravelResponse() {
-		return new MainTravelResponse(MAIN_TRAVEL_ID, TITLE, START_DATE_STRING, END_DATE_STRING, IS_VISIBLE,
-			IS_COMPLETED, LIKE_NUM);
+		return new MainTravelResponse(MAIN_TRAVEL_ID, TITLE, START_DATE_STRING, END_DATE_STRING, CREATED_AT_STRING,
+			IS_VISIBLE, IS_COMPLETED, LIKE_NUM);
 	}
 
 	public static MainTravelDto getMainTravelDto() {
-		return new MainTravelDto(MAIN_TRAVEL_ID, TITLE, START_DATE_STRING, END_DATE_STRING, IS_VISIBLE, IS_COMPLETED,
-			LIKE_NUM);
+		return new MainTravelDto(MAIN_TRAVEL_ID, TITLE, START_DATE_STRING, END_DATE_STRING, CREATED_AT_STRING,
+			IS_VISIBLE, IS_COMPLETED, LIKE_NUM);
 	}
 
 	public static MainTravelSaveDto getMainTravelSaveDto() {
